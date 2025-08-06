4c5a7a20e68ad6f80062a68846348ce9ab337a55
heskalde <henrik.skaldebo@gmail.com>
2025-08-06 16:46:51 +0200

    More robust orientRotation.m handling more cases correctly

--- functions/orientRotation.m
+++ functions/orientRotation.m
@@ -1,42 +1,48 @@
 function rotIm = orientRotation(imageData,rawObj, visuParam)
     % Rotates and flips the image matrix according to geometry data from the method file
 
-    % view: 
-    %   - LAX also contain 'LAX' in their protocol name
-    %   - 'CS_191021' is the unique string in the SAX protocol
-    view                                = visuParam.VisuAcquisitionProtocol;
-
     % Should be one of H_F, A_P or L_R
     readOrient                          = rawObj.Method.PVM_SPackArrReadOrient;
+    % Should be one of sagittal, coronal and axial
+    sliceOrient                         = rawObj.Method.PVM_SPackArrSliceOrient;
 
-    k                                   = 0;     % constant for how many 90 degree rotations to do
-    f                                   = false; % boolean for image flipping
+    k                                   = 0;    % constant for how many 90 degree rotations to do
+    f                                   = 0;    % constant for direction of flip, f=0 means no flip happens
 
-    if contains(view, 'LAX')
+    if contains(sliceOrient, 'sagittal')
         if contains(readOrient, 'H_F')
             k = 2;
         end
+
         if contains(readOrient, 'A_P')
             k = 1;
         end
-        if contains(readOrient, 'L_R') % have not seen this case, but if it appears for coronal LAX images hopefully it works similarly to A_P
+    end
+
+    if contains(sliceOrient, 'coronal')
+        if contains(readOrient, 'H_F')
+            k = 2;
+        end
+        if contains(readOrient, 'L_R')
             k = 1;
+            f = 2;
         end
     end
-    if contains(view, 'CS_191021')
+
+    if contains(sliceOrient, 'axial')
         if contains(readOrient, 'A_P')
             k = 2;
         end
         if contains(readOrient, 'L_R')
             k = 1;
-            f = true;
+            f = 2;
         end
     end
     
     imageData                           = rot90(imageData,k);
     
-    if f == true
-        imageData                       = flip(imageData,2);
+    if f ~= 0
+        imageData                       = flip(imageData,f);
     end
 
     rotIm = imageData;
